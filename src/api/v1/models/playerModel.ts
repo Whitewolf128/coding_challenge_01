41bd95b9505af8a7c150d68fda82e32ffec84d67
Zachary Ross <zross@academic.rrc.ca>
2026-09-14 09:38:06 -0500

    got the interface working

---
+++ src/api/v1/models/playerModel.ts
@@ -0,0 +1,7 @@
+export interface Player {
+    id?: number;
+    name: string;
+    wins: number;
+    losses: number;
+    totalScore: number;
+}
---
+++ src/api/v1/services/playerServices.ts
@@ -0,0 +1,18 @@
+import {Player} from '../models/playerModel';
+
+const COLLECTION = "player";
+/**
+ * Updates an existing post.
+ * @param {Post} postData - The updated post data.
+ * @returns {Promise<void>}
+ * @throws {Error} - If validation or repository operation fails.
+ */
+
+//export const getAllPlayers = async (): Promise<Player[]> => {
+    //try{
+        //const player: Player[] 
+    //}
+    //catch (error:unknown) {
+        
+    //}
+//}
