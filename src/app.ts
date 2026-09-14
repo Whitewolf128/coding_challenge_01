cb12e2486a81fbb152f7ee386a035751d487b7b2
Elijah Gott <egott@academic.rrc.ca>
2026-09-14 10:27:56 -0500

    Feat: Add app.ts imports and code.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 // import the express application and type definition
 import express, { Express } from "express";
+import playerRoute from "./api/v1/routes/playerRoute"
 
 // initialize the express application
 const app: Express = express();
+app.use(express.json());
 
 // Interface for health check response
 interface HealthCheckResponse {
@@ -30,6 +32,8 @@ app.get("/api/v1/health", (req, res) => {
 
     res.json(healthData);
 });
+
+app.use("/api/v1", playerRoute);
 
 // export app and server for testing
 export default app;
